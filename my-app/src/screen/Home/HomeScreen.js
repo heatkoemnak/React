7ac9786fe35222580889a7ecee3a8d35b54b8240
Heat Koemnak <heatkimnak@gmail.com>
2023-05-04 01:59:58 +0700

    added footer

--- my-app/src/screen/Home/HomeScreen.js
+++ my-app/src/screen/Home/HomeScreen.js
@@ -54,7 +54,6 @@ import watch8 from "../asset/watch8.webp";
 import watch9 from "../asset/watch9.webp";
 import watch10 from "../asset/watch10.webp";
 
-
 import related1 from "../asset/related_to_recycle1.jpg";
 import related2 from "../asset/related_to_recycle2.jpg";
 import related3 from "../asset/related_to_recycle3.jpg";
@@ -66,11 +65,6 @@ import related8 from "../asset/related_to_recycle8.jpg";
 import related9 from "../asset/related_to_recycle9.jpg";
 import related10 from "../asset/related_to_recycle10.jpg";
 import related11 from "../asset/related-to-recycle11.jpg";
-
-
-
-
-
 
 const HomeScreen = () => {
   const [selectImage, setSelectImage] = useState(1);
@@ -405,12 +399,7 @@ function Shoes() {
       <section class="Product" id="product">
         <div class="product-container" id="product-container">
           <div class="product-info" id="product-info">
-            <img
-              class="product-image"
-              id="product-image"
-              src={watch5}
-              alt=""
-            />
+            <img class="product-image" id="product-image" src={watch5} alt="" />
             <div class="product-text">
               <div class="name-price">
                 <h3 class="product-name">Oris Aqius Date</h3>
@@ -432,12 +421,7 @@ function Shoes() {
             </div>
           </div>
           <div class="product-info" id="product-info">
-            <img
-              class="product-image"
-              id="product-image"
-              src={watch6}
-              alt=""
-            />
+            <img class="product-image" id="product-image" src={watch6} alt="" />
             <div class="product-text">
               <div class="name-price">
                 <h3 class="product-name">Suunto 5 Peak Mist Blue</h3>
@@ -460,12 +444,7 @@ function Shoes() {
           </div>
 
           <div class="product-info" id="product-info">
-            <img
-              class="product-image"
-              id="product-image"
-              src={watch7}
-              alt=""
-            />
+            <img class="product-image" id="product-image" src={watch7} alt="" />
             <div class="product-text">
               <div class="name-price">
                 <h3 class="product-name">Radley ladies Strap</h3>
@@ -487,12 +466,7 @@ function Shoes() {
             </div>
           </div>
           <div class="product-info" id="product-info">
-            <img
-              class="product-image"
-              id="product-image"
-              src={watch8}
-              alt=""
-            />
+            <img class="product-image" id="product-image" src={watch8} alt="" />
             <div class="product-text">
               <div class="name-price">
                 <h3 class="product-name">YA157304</h3>
@@ -516,165 +490,193 @@ function Shoes() {
         </div>
       </section>
       <section class="Product" id="product">
-      <div class="product-container" id="product-container">
-        <div class="product-info" id="product-info">
-          <img
-            class="product-image"
-            id="product-image"
-            src={bag8}
-            alt=""
-          />
-          <div class="product-text">
-            <div class="name-price">
-              <h3 class="product-name">Jecket Treamvelet</h3>
-              <p class="product-price">150.0$</p>
-            </div>
-            <p class="product-details">
-              Product product-details dverbgern erhehtr erehge
-            </p>
-            <div class="product-card" id="card">
-              <div class="star">
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bxs-star-half"></i>
+        <div class="product-container" id="product-container">
+          <div class="product-info" id="product-info">
+            <img class="product-image" id="product-image" src={bag8} alt="" />
+            <div class="product-text">
+              <div class="name-price">
+                <h3 class="product-name">Jecket Treamvelet</h3>
+                <p class="product-price">150.0$</p>
               </div>
-              <i class="bx bx-shopping-bag add-to-cart"></i>
+              <p class="product-details">
+                Product product-details dverbgern erhehtr erehge
+              </p>
+              <div class="product-card" id="card">
+                <div class="star">
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bxs-star-half"></i>
+                </div>
+                <i class="bx bx-shopping-bag add-to-cart"></i>
+              </div>
+            </div>
+          </div>
+          <div class="product-info" id="product-info">
+            <img class="product-image" id="product-image" src={bag7} alt="" />
+            <div class="product-text">
+              <div class="name-price">
+                <h3 class="product-name">Mini Luvis</h3>
+                <p class="product-price">80.0$</p>
+              </div>
+              <p class="product-details">
+                Product product-details dverbgern erhehtr erehge
+              </p>
+              <div class="product-card" id="card">
+                <div class="star">
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bxs-star-half"></i>
+                </div>
+                <i class="bx bx-shopping-bag add-to-cart"></i>
+              </div>
+            </div>
+          </div>
+
+          <div class="product-info" id="product-info">
+            <img class="product-image" id="product-image" src={bag5} alt="" />
+            <div class="product-text">
+              <div class="name-price">
+                <h3 class="product-name">Li Maton</h3>
+                <p class="product-price">60.0$</p>
+              </div>
+              <p class="product-details">
+                Product product-details dverbgern erhehtr erehge
+              </p>
+              <div class="product-card" id="card">
+                <div class="star">
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bxs-star-half"></i>
+                </div>
+                <i class="bx bx-shopping-bag add-to-cart"></i>
+              </div>
+            </div>
+          </div>
+          <div class="product-info" id="product-info">
+            <img class="product-image" id="product-image" src={bag6} alt="" />
+            <div class="product-text">
+              <div class="name-price">
+                <h3 class="product-name">Luvis Jecket</h3>
+                <p class="product-price">30.0$</p>
+              </div>
+              <p class="product-details">
+                Product product-details dverbgern erhehtr erehge
+              </p>
+              <div class="product-card" id="card">
+                <div class="star">
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bx-star"></i>
+                  <i class="bx bxs-star-half"></i>
+                </div>
+                <i class="bx bx-shopping-bag add-to-cart"></i>
+              </div>
             </div>
           </div>
         </div>
-        <div class="product-info" id="product-info">
-          <img
-            class="product-image"
-            id="product-image"
-            src={bag7}
-            alt=""
-          />
-          <div class="product-text">
-            <div class="name-price">
-              <h3 class="product-name">Mini Luvis</h3>
-              <p class="product-price">80.0$</p>
+      </section>
+
+      <div class="Related">
+        <h2>Related Collection</h2>
+        <div class="related-container">
+          <div class="wrape-content">
+            <div class="image">
+              <img src={related11} alt="" />
             </div>
-            <p class="product-details">
-              Product product-details dverbgern erhehtr erehge
-            </p>
-            <div class="product-card" id="card">
-              <div class="star">
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bxs-star-half"></i>
+            <div class="related-text">
+              <h3>Recycle</h3>
+              <div class="photos-related">
+                <p>693 photos</p>
+                <p>Created by Mel Hammer</p>
               </div>
-              <i class="bx bx-shopping-bag add-to-cart"></i>
+            </div>
+          </div>
+        </div>
+        <div class="related-container">
+          <div class="wrape-content">
+            <div class="image">
+              <img src={related2} alt="" />
+            </div>
+            <div class="related-text">
+              <h3>Texture/Plastic</h3>
+              <div class="photos-related">
+                <p>1.1k photos</p>
+                <p>Created by Mixer X</p>
+              </div>
             </div>
           </div>
         </div>
 
-        <div class="product-info" id="product-info">
-          <img
-            class="product-image"
-            id="product-image"
-            src={bag5}
-            alt=""
-          />
-          <div class="product-text">
-            <div class="name-price">
-              <h3 class="product-name">Li Maton</h3>
-              <p class="product-price">60.0$</p>
+        <div class="related-container">
+          <div class="wrape-content">
+            <div class="image">
+              <img src={related5} alt="" />
             </div>
-            <p class="product-details">
-              Product product-details dverbgern erhehtr erehge
-            </p>
-            <div class="product-card" id="card">
-              <div class="star">
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bxs-star-half"></i>
+            <div class="related-text">
+              <h3>Going Green, Save the Earth, Climate Change</h3>
+              <div class="photos-related">
+                <p>2.3k photos</p>
+                <p>Created by Gen Alert</p>
               </div>
-              <i class="bx bx-shopping-bag add-to-cart"></i>
-            </div>
-          </div>
-        </div>
-        <div class="product-info" id="product-info">
-          <img
-            class="product-image"
-            id="product-image"
-            src={bag6}
-            alt=""
-          />
-          <div class="product-text">
-            <div class="name-price">
-              <h3 class="product-name">Luvis Jecket</h3>
-              <p class="product-price">30.0$</p>
-            </div>
-            <p class="product-details">
-              Product product-details dverbgern erhehtr erehge
-            </p>
-            <div class="product-card" id="card">
-              <div class="star">
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bx-star"></i>
-                <i class="bx bxs-star-half"></i>
-              </div>
-              <i class="bx bx-shopping-bag add-to-cart"></i>
             </div>
           </div>
         </div>
       </div>
-    </section>
-
-    <div class="Related">
-      <h2>Related Collection</h2>
-      <div class="related-container">
-        <div class="wrape-content">
-          <div class="image">
-            <img src={related11} alt="" />
-          </div>
-          <div class="related-text">
-            <h3>Recycle</h3>
-            <div class="photos-related">
-              <p>693 photos</p>
-              <p>Created by Mel Hammer</p>
+      <footer>
+        <div class="footer-container">
+          <div class="footer-menu">
+            <div class="footer-menu-item">
+              <h2>Menu</h2>
+              <a href="#">Home</a>
+              <a href="#">About</a>
+              <a href="#">Service</a>
+              <a href="#">Product</a>
+              <a href="#">Contact</a>
+            </div>
+            <div class="footer-menu-item">
+              <h2>Contact Info</h2>
+              <a href="#">+855 977 304 279</a>
+              <a href="#">heatkoemnak@gamil.com</a>
+              <a href="#">Pnom penh,Cambodia-400104</a>
+            </div>
+            <div class="footer-menu-item">
+              <h2>Follow Us</h2>
+              <a href="#">Facebook</a>
+              <a href="#">Instagram</a>
+              <a href="#">Twitter</a>
+              <a href="#">Youtube</a>
+              <a href="#">Linkedin</a>
+            </div>
+            <div class="footer-menu-item">
+              <h2>Legal</h2>
+              <a href="#">Terms of use</a>
+              <a href="#">Privacy policy</a>
+              <a href="#">Cookies</a>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="related-container">
-        <div class="wrape-content">
-          <div class="image">
-            <img src={related2} alt="" />
-          </div>
-          <div class="related-text">
-            <h3>Texture/Plastic</h3>
-            <div class="photos-related">
-              <p>1.1k photos</p>
-              <p>Created by Mixer X</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="related-container">
-        <div class="wrape-content">
-          <div class="image">
-            <img src={related5} alt="" />
-          </div>
-          <div class="related-text">
-            <h3>Going Green, Save the Earth, Climate Change</h3>
-            <div class="photos-related">
-              <p>2.3k photos</p>
-              <p>Created by Gen Alert</p>
-            </div>
+          <div class="footer-social-media">
+            <a href="#">
+              <i class="bx bxl-facebook"></i>
+            </a>
+            <a href="#">
+              <i class="bx bxl-instagram"></i>
+            </a>
+            <a href="#">
+              <i class="bx bxl-twitter"></i>
+            </a>
+            <a href="#">
+              <i class="bx bxl-youtube"></i>
+            </a>
           </div>
         </div>
-      </div>
-    </div>
+      </footer>
     </>
   );
 }
