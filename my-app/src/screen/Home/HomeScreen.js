0a5193be8a7854f0a9943ababe4fef677bf69c7d
Heat Koemnak <heatkimnak@gmail.com>
2023-05-04 00:59:07 +0700

    added materails

--- my-app/src/screen/Home/HomeScreen.js
+++ my-app/src/screen/Home/HomeScreen.js
@@ -6,6 +6,15 @@ import image2 from "../asset/s002.png";
 import image3 from "../asset/s003.png";
 import image4 from "../asset/s001.png";
 
+import material1 from "../asset/1.webp";
+import material2 from "../asset/1.webp";
+import material3 from "../asset/2.jpg";
+import material4 from "../asset/3.jpg";
+import material5 from "../asset/4.jfif";
+import material6 from "../asset/5.jpg";
+import material7 from "../asset/6.jpg";
+import material8 from "../asset/1.webp";
+
 const HomeScreen = () => {
   const [selectImage, setSelectImage] = useState(1);
   const [allImage, setAllImage] = useState([image1, image2, image3, image4]);
@@ -24,34 +33,82 @@ const HomeScreen = () => {
   }, [selectImage]);
 
   return (
-    <div className="campagn">
-      <div className="feature">
-        <Link className="feature-list" to="/">
-          Sell Your Products
-        </Link>
-        <Link className="feature-list" to="/Service">
-          Service Provider
-        </Link>
-        <Link className="feature-list" to="/Shop">
-          Nearby Enterprise
-        </Link>
-        <div className="location">
-          <i className="bx bx-location-plus"></i>
-          <i className="bx bx-chevron-up"></i>
-          <p>Location</p>
-          <p>សង្កាត់ទឹកថ្លា Sangkat Tuek Thla ភ្នំពេញ</p>
-          <p>10km 30s</p>
-          <i className="bx bx-chevron-down "></i>
+    <>
+      <div className="campagn">
+        <div className="feature">
+          <Link className="feature-list" to="/">
+            Sell Your Products
+          </Link>
+          <Link className="feature-list" to="/Service">
+            Service Provider
+          </Link>
+          <Link className="feature-list" to="/Shop">
+            Nearby Enterprise
+          </Link>
+          <div className="location">
+            <i className="bx bx-location-plus"></i>
+            <i className="bx bx-chevron-up"></i>
+            <p>Location</p>
+            <p>សង្កាត់ទឹកថ្លា Sangkat Tuek Thla ភ្នំពេញ</p>
+            <p>10km 30s</p>
+            <i className="bx bx-chevron-down "></i>
+          </div>
+          <Link className="feature-list" to="/Contact">
+            ALL PRODUCTS TYPE
+          </Link>
         </div>
-        <Link className="feature-list" to="/Contact">
-          ALL PRODUCTS TYPE
-        </Link>
+        <div className="banner">
+          <img src={allImage[selectImage]} alt="all" />
+        </div>
       </div>
-      <div className="banner">
-        <img src={allImage[selectImage]} alt="all" />
+      <Materails />
+
+      <div className="search-product">
+        <input type="text" placeholder="Search Product" />
+        <button className="search-btn">Search</button>
+      </div>
+    </>
+  );
+};
+
+function Materails() {
+  return (
+    <div className="materails">
+      <div className="materail">
+        <img src={material1} alt="image1" />
+        <p>Plastic</p>
+      </div>
+      <div className="materail">
+        <img src={material2} alt="image2" />
+        <p>Plastic</p>
+      </div>
+      <div className="materail">
+        <img src={material3} alt="image3" />
+        <p>Plastic</p>
+      </div>
+      <div className="materail">
+        <img src={material4} alt="image4" />
+        <p>Plastic</p>
+      </div>
+      <div className="materail">
+        <img src={material5} alt="image5" />
+        <p>Plastic</p>
+      </div>
+      <div className="materail">
+        <img src={material6} alt="image6" />
+        <p>Plastic</p>
+      </div>
+      <div className="materail">
+        <img src={material7} alt="image7" />
+        <p>Plastic</p>
+      </div>
+
+      <div className="materail">
+        <img src={material8} alt="image8" />
+        <p>Plastic</p>
       </div>
     </div>
   );
-};
+}
 
 export default HomeScreen;
