c9a72c5bd778f1e9da0e99cb231f78073dae9b98
Heat Koemnak <heatkimnak@gmail.com>
2023-05-04 01:53:35 +0700

    added related

--- my-app/src/screen/Home/HomeScreen.js
+++ my-app/src/screen/Home/HomeScreen.js
@@ -55,6 +55,20 @@ import watch9 from "../asset/watch9.webp";
 import watch10 from "../asset/watch10.webp";
 
 
+import related1 from "../asset/related_to_recycle1.jpg";
+import related2 from "../asset/related_to_recycle2.jpg";
+import related3 from "../asset/related_to_recycle3.jpg";
+import related4 from "../asset/related_to_recycle4.jpg";
+import related5 from "../asset/related_to_recycle5.jpg";
+import related6 from "../asset/related_to_recycle6.jpg";
+import related7 from "../asset/related_to_recycle7.jpg";
+import related8 from "../asset/related_to_recycle8.jpg";
+import related9 from "../asset/related_to_recycle9.jpg";
+import related10 from "../asset/related_to_recycle10.jpg";
+import related11 from "../asset/related-to-recycle11.jpg";
+
+
+
 
 
 
@@ -614,6 +628,53 @@ function Shoes() {
         </div>
       </div>
     </section>
+
+    <div class="Related">
+      <h2>Related Collection</h2>
+      <div class="related-container">
+        <div class="wrape-content">
+          <div class="image">
+            <img src={related11} alt="" />
+          </div>
+          <div class="related-text">
+            <h3>Recycle</h3>
+            <div class="photos-related">
+              <p>693 photos</p>
+              <p>Created by Mel Hammer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="related-container">
+        <div class="wrape-content">
+          <div class="image">
+            <img src={related2} alt="" />
+          </div>
+          <div class="related-text">
+            <h3>Texture/Plastic</h3>
+            <div class="photos-related">
+              <p>1.1k photos</p>
+              <p>Created by Mixer X</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="related-container">
+        <div class="wrape-content">
+          <div class="image">
+            <img src={related5} alt="" />
+          </div>
+          <div class="related-text">
+            <h3>Going Green, Save the Earth, Climate Change</h3>
+            <div class="photos-related">
+              <p>2.3k photos</p>
+              <p>Created by Gen Alert</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   );
 }
