010da167efdf55ba226ff3ad53258b5e1f6dca15
Heat Koemnak <heatkimnak@gmail.com>
2023-05-04 18:02:47 +0700

    added more product

--- my-app/src/screen/product/Fashion/FashionScreen.js
+++ my-app/src/screen/product/Fashion/FashionScreen.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 //import fashion img
 import fashion1 from "../../../screen/asset/fashion1.jpg";
 import fashion2 from "../../../screen/asset/fashion2.webp";
@@ -9,6 +11,18 @@ import fashion6 from "../../../screen/asset/fashion6.jpg";
 import fashion7 from "../../../screen/asset/fashion7.jpg";
 import fashion8 from "../../../screen/asset/fashion8.jpg";
 import fashion9 from "../../../screen/asset/fashion9.jpg";
+
+import related1 from "../../../screen/asset/related_to_recycle1.jpg";
+import related2 from "../../../screen/asset/related_to_recycle2.jpg";
+import related3 from "../../../screen/asset/related_to_recycle3.jpg";
+import related4 from "../../../screen/asset/related_to_recycle4.jpg";
+import related5 from "../../../screen/asset/related_to_recycle5.jpg";
+import related6 from "../../../screen/asset/related_to_recycle6.jpg";
+import related7 from "../../../screen/asset/related_to_recycle7.jpg";
+import related8 from "../../../screen/asset/related_to_recycle8.jpg";
+import related9 from "../../../screen/asset/related_to_recycle9.jpg";
+import related10 from "../../../screen/asset/related_to_recycle10.jpg";
+import related11 from "../../../screen/asset/related-to-recycle11.jpg";
 
 function FashionScreen() {
   return (
@@ -342,7 +356,111 @@ function FashionScreen() {
           </div>
         </div>
       </section>
+      <Related />
+      <Footer />
     </>
+  );
+}
+function Footer() {
+  return (
+    <footer>
+      <div class="footer-container">
+        <div class="footer-menu">
+          <div class="footer-menu-item">
+            <h2>Menu</h2>
+            <Link href="#">Home</Link>
+            <Link href="#">About</Link>
+            <Link href="#">Service</Link>
+            <Link href="#">Product</Link>
+            <Link href="#">Contact</Link>
+          </div>
+          <div class="footer-menu-item">
+            <h2>Contact Info</h2>
+            <Link href="#">+855 977 304 279</Link>
+            <Link href="#">heatkoemnak@gamil.com</Link>
+            <Link href="#">Pnom penh,Cambodia-400104</Link>
+          </div>
+          <div class="footer-menu-item">
+            <h2>Follow Us</h2>
+            <Link href="#">Facebook</Link>
+            <Link href="#">Instagram</Link>
+            <Link href="#">Twitter</Link>
+            <Link href="#">Youtube</Link>
+            <Link href="#">Linkedin</Link>
+          </div>
+          <div class="footer-menu-item">
+            <h2>Legal</h2>
+            <Link href="#">Terms of use</Link>
+            <Link href="#">Privacy policy</Link>
+            <Link href="#">Cookies</Link>
+          </div>
+        </div>
+        <div class="footer-social-media">
+          <Link href="#">
+            <i class="bx bxl-facebook"></i>
+          </Link>
+          <Link href="#">
+            <i class="bx bxl-instagram"></i>
+          </Link>
+          <Link href="#">
+            <i class="bx bxl-twitter"></i>
+          </Link>
+          <Link href="#">
+            <i class="bx bxl-youtube"></i>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
+function Related() {
+  return (
+    <div class="Related">
+      <h2>Related Collection</h2>
+      <div class="related-container">
+        <div class="wrape-content">
+          <div class="image">
+            <img src={related11} alt="" />
+          </div>
+          <div class="related-text">
+            <h3>Recycle</h3>
+            <div class="photos-related">
+              <p>693 photos</p>
+              <p>Created by Mel Hammer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="related-container">
+        <div class="wrape-content">
+          <div class="image">
+            <img src={related2} alt="" />
+          </div>
+          <div class="related-text">
+            <h3>Texture/Plastic</h3>
+            <div class="photos-related">
+              <p>1.1k photos</p>
+              <p>Created by Mixer X</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="related-container">
+        <div class="wrape-content">
+          <div class="image">
+            <img src={related5} alt="" />
+          </div>
+          <div class="related-text">
+            <h3>Going Green, Save the Earth, Climate Change</h3>
+            <div class="photos-related">
+              <p>2.3k photos</p>
+              <p>Created by Gen Alert</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
