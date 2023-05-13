aee2f236ec0c34835b6d91732590ca94cd043f0f
Heat Koemnak <heatkimnak@gmail.com>
2023-05-13 17:07:24 +0700

    added search product to homepage

---
+++ my-app/src/component/searchProduct.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function searchProduct() {
+  return (
+    <div className="search-product">
+      <input type="text" placeholder="Search Product" />
+      <button className="search-btn">Search</button>
+    </div>
+  );
+}
+
+export default searchProduct;
