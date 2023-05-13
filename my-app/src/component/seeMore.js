6c420b42204ef43954c7473760455c4229fd0acc
Heat Koemnak <heatkimnak@gmail.com>
2023-05-13 17:07:58 +0700

    added see more to homepage

---
+++ my-app/src/component/seeMore.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function seeMore() {
+  return (
+    <div className="Popular" id="popular">
+      <div className="popular-text">
+        <div className="products-type">
+          <p>Trending Product</p>
+        </div>
+        <p>See more</p>
+      </div>
+    </div>
+  );
+}
+
+export default seeMore;
