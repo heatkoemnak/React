a72ddcdb57d21806c1cfe2d8148b5595a3ced46d
Heat Koemnak <heatkimnak@gmail.com>
2023-05-29 17:12:47 +0700

    deleted

--- app/src/component/Product.js
+++ app/src/component/Product.js
@@ -21,11 +21,11 @@ const Product = ({ product, addToCart }) => {
                 <div className="product-details">{productItem.description}</div>
                 <div className="product-card" id="card">
                   <div className="star">
-                    <i className="bx bx-star"></i>
-                    <i className="bx bx-star"></i>
-                    <i className="bx bx-star"></i>
-                    <i className="bx bx-star"></i>
-                    <i className="bx bxs-star-half"></i>
+                    <i class="fa fa-star bx-star"></i>
+                    <i class="fa fa-star bx-star"></i>
+                    <i class="fa fa-star bx-star"></i>
+                    <i class="fa fa-star bx-star"></i>
+                    <i class="fa fa-star bx-star-half"></i>
                   </div>
                   <i
                     onClick={() => addToCart(productItem)}
