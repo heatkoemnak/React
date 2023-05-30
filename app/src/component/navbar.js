9567e2fa892756d54f0c8a371915d6dc8f27a33d
Heat Koemnak <heatkimnak@gmail.com>
2023-05-31 01:29:08 +0700

    add ed useState

--- app/src/component/navbar.js
+++ app/src/component/navbar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import logo from "../screen/asset/anti.png";
