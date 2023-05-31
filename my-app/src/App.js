872e09930a21ef2ce9d954435fb12e8dc84cfc65
Heat Koemnak <heatkimnak@gmail.com>
2023-05-31 23:32:09 +0700

    import reander file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+
 import Cart from "./Pages/Cart";
 import Navbar from "./components/Navbar";
 import MenuProduct from "./components/MenuProduct";
+import Fashion from "./Pages/Fashion";
+import Shoes from "./Pages/Shoes";
+import Bag from "./Pages/Bag";
+import Watch from "./Pages/Watch";
+import Toy from "./Pages/Toy";
+import Accessary from "./Pages/Accessary";
 
 function App() {
   return (
@@ -18,6 +25,12 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/cart" element={<Cart />} />
+              <Route path="/fashion" element={<Fashion />} />
+              <Route path="/shoes" element={<Shoes />} />
+              <Route path="/bag" element={<Bag />} />
+              <Route path="/watch" element={<Watch />} />
+              <Route path="/toy" element={<Toy />} />
+              <Route path="/accessary" element={<Accessary />} />
             </Routes>
           </div>
         </div>
