51544a690b7d61b72e0b2ad3d802a83fb24ebae5
Heat Koemnak <heatkimnak@gmail.com>
2023-06-05 20:43:50 +0700

    added shoes ,bag,watch to cart

--- my-app/src/CartContext.js
+++ my-app/src/CartContext.js
@@ -1,17 +1,30 @@
 import React, { createContext, useState } from "react";
 import PRODUCTS from "./HomeProduct";
 import FASHION from "./FashoinProduct";
+import SHOES from "./ShoesProduct";
+import BAG from "./BagProduct";
+import WATCH from "./WatchProduct";
+
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [product] = useState(PRODUCTS);
   const [fashion] = useState(FASHION);
+  const [shoes] = useState(SHOES);
+  const [bag] = useState(BAG);
+  const [watch] = useState(WATCH);
+
   const [cart, setCart] = useState([]);
-  const addToCart = (product, fashion) => {
-    setCart([...cart, { ...product, ...fashion, quantity: 1 }]);
+  const addToCart = (product, fashion, shoes, bag, watch) => {
+    setCart([
+      ...cart,
+      { ...product, ...fashion, ...shoes, ...bag, ...watch, quantity: 1 },
+    ]);
   };
   return (
-    <CartContext.Provider value={{ cart, product, fashion, addToCart }}>
+    <CartContext.Provider
+      value={{ cart, product, fashion, shoes, bag, watch, addToCart }}
+    >
       {children}
     </CartContext.Provider>
   );
