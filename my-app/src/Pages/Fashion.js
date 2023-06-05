76bbff4788651577cfa5ade595972cdab43a36c2
Heat Koemnak <heatkimnak@gmail.com>
2023-06-05 20:50:18 +0700

    modified

--- my-app/src/Pages/Fashion.js
+++ my-app/src/Pages/Fashion.js
@@ -1,18 +1,43 @@
-import React, { useState } from "react";
-import Product from "../components/Product";
-import Cart from "../Pages/Cart";
-import "../Pages/CSS/fashion.css";
-import CartContext from "../CartContext";
+import React from "react";
+import "./CSS/home.css";
+import "../style/Product.css";
 import { useContext } from "react";
-
+import CartContext from "../CartContext";
 function Fashion() {
-  const { cart, fashion, addToCart } = useContext(CartContext);
+  const { fashion, addToCart } = useContext(CartContext);
 
   return (
-    <div className="fashion">
-      <Product product={fashion} addToCart={addToCart} />;
-      <Cart cart={cart}></Cart>
-    </div>
+    <>
+      <div className="Product">
+        {fashion.map((fashionItems) => {
+          return (
+            <>
+              <div className="product-container" key={fashionItems.id}>
+                <div className="product-image">
+                  <img
+                    src={fashionItems.image}
+                    alt={fashionItems.name}
+                    width={250}
+                    s
+                    height={250}
+                  />
+                </div>
+                <div className="product-name">{fashionItems.name}</div>
+                <div className="product-price">${fashionItems.price}</div>
+                <div className="product-description">
+                  {fashionItems.description}
+                </div>
+                <div className="product-action">
+                  <button onClick={() => addToCart(fashionItems)}>
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
