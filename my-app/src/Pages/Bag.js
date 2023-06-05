6ab086e2516978c0d039dc3fd7ac61cce4e536fd
Heat Koemnak <heatkimnak@gmail.com>
2023-06-05 20:47:29 +0700

    added Bages

--- my-app/src/Pages/Bag.js
+++ my-app/src/Pages/Bag.js
@@ -1,15 +1,42 @@
-import BAG from "../BagProduct";
-import "../Pages/CSS/fashion.css";
-
-import React, { useState } from "react";
-import Product from "../components/Product";
+import React from "react";
+import "./CSS/home.css";
+import "../style/Product.css";
+import { useContext } from "react";
+import CartContext from "../CartContext";
 function Bag() {
-  const [bag] = useState(BAG);
+  const { bag, addToCart } = useContext(CartContext);
 
   return (
-    <div>
-      <Product product={bag} />;
-    </div>
+    <>
+      <div className="Product">
+        {bag.map((bagItems) => {
+          return (
+            <>
+              <div className="product-container" key={bagItems.id}>
+                <div className="product-image">
+                  <img
+                    src={bagItems.image}
+                    alt={bagItems.name}
+                    width={250}
+                    height={250}
+                  />
+                </div>
+                <div className="product-name">{bagItems.name}</div>
+                <div className="product-price">{bagItems.price}</div>
+                <div className="product-description">
+                  {bagItems.description}
+                </div>
+                <div className="product-action">
+                  <button onClick={() => addToCart(bagItems)}>
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
