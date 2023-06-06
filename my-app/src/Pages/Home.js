b2a469fa4f083e4d96b928a0b7523f46aff13b51
Heat Koemnak <heatkimnak@gmail.com>
2023-06-06 22:04:36 +0700

    remove cart

--- my-app/src/Pages/Home.js
+++ my-app/src/Pages/Home.js
@@ -3,18 +3,16 @@ import "./CSS/home.css";
 import Product from "../components/Product";
 import Slider from "../components/Slider";
 import "../style/Product.css";
-import Cart from "../Pages/Cart";
 import { useContext } from "react";
 import CartContext from "../CartContext";
 
 function Home() {
-  const { cart, product, addToCart } = useContext(CartContext);
+  const { product, addToCart } = useContext(CartContext);
 
   return (
     <>
       <div className="home">
         <Slider />
-        <Cart cart={cart}></Cart>
       </div>
       <Product product={product} addToCart={addToCart}></Product>
     </>
