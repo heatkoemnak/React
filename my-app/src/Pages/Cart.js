6aae6c02e4434fe36dc0638db8a37d2fee694659
Heat Koemnak <heatkimnak@gmail.com>
2023-06-12 02:39:24 +0700

    modified

--- my-app/src/Pages/Cart.js
+++ my-app/src/Pages/Cart.js
@@ -2,25 +2,55 @@ import React from "react";
 import "../Pages/CSS/cart.css";
 import { useContext } from "react";
 import CartContext from "../CartContext";
+import { Link } from "react-router-dom";
 function Cart() {
   const { cart } = useContext(CartContext);
 
   return (
-    <div className="cart-list">
-      <h2>YOUR CART</h2>
-      {cart.map((cartItem) => {
-        return (
-          <div className="cart-item" key={cartItem.id}>
-            <div>
-              <img src={cartItem.image} alt="" width={60} height={60}></img>
-            </div>
-            <div>{cartItem.name}</div>
-            <div>{cartItem.price}</div>
-            <div>{cartItem.quantity}</div>
+    <>
+      <p>
+        <Link to="/">Continue Shopping</Link>
+      </p>
+      <div className="cart">
+        <div className="cart-list">
+          <div className="cart-header">
+            <i class="bx bxl-shopify"></i>
+            <h2>CART PREVIEW</h2>
           </div>
-        );
-      })}
-    </div>
+
+          {cart.map((cartItem) => {
+            return (
+              <div className="cart-item" key={cartItem.id}>
+                <div className="cart-image">
+                  <img
+                    src={cartItem.image}
+                    alt=""
+                    width={120}
+                    height={120}
+                  ></img>
+                  <div className="cart-text">
+                    <div className="cart-item-name">{cartItem.name}</div>
+                    <div className="cart-item-price">${cartItem.price}</div>
+                    <div>{cartItem.quantity}</div>
+                  </div>
+                </div>
+                <div className="delete">
+                  <i class="bx bx-x"></i>
+                </div>
+              </div>
+            );
+          })}
+          <div className="total-price">
+            <div className="Total">Total</div>
+            <div className="price">$0.00</div>
+          </div>
+        </div>
+        
+      </div>
+      <div className="payment">
+        <h2>PAYMENT</h2>
+      </div>
+    </>
   );
 }
 
