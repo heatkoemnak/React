d1e179e905f5f647dd6308dd6d2c4c15ef80a934
Heat Koemnak <heatkimnak@gmail.com>
2023-06-27 02:04:32 +0700

    added content

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1e1e1e",
+        secondary: "#2d2d2d",
+      },
+      
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
