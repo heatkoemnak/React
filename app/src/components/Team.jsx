9b606b5e16536eb6ea7d8b4a0627276a670c69ff
Heat Koemnak <heatkimnak@gmail.com>
2023-06-29 22:59:41 +0700

    custom team

--- app/src/components/Team.jsx
+++ app/src/components/Team.jsx
@@ -1,5 +1,64 @@
 import React from "react";
 
 export default function Team() {
-  return <div>Team Page</div>;
+  return (
+    <>
+      <div>
+        <div class="flex items-center space-x-2 text-base">
+          <h4 class="font-semibold text-slate-900">Contributors</h4>
+          <span class="rounded-full bg-slate-100 px-2 py-1 text-xs font-semibold text-slate-700">
+            204
+          </span>
+        </div>
+        <div class="mt-3 flex -space-x-2 overflow-hidden">
+          <img
+            class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <img
+            class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <img
+            class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+            alt=""
+          />
+          <img
+            class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+          <img
+            class="inline-block h-12 w-12 rounded-full ring-2 ring-white"
+            src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            alt=""
+          />
+        </div>
+        <div class="mt-3 text-sm font-medium">
+          <a href="#" class="text-blue-500">
+            + 198 others
+          </a>
+        </div>
+      </div>
+      <div class="py-8 px-8 max-w-sm  bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex  sm:space-y-0 sm:space-x-6">
+        <img
+          class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+          src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt="Woman's Face"
+        />
+        <div class="text-center space-y-2 sm:text-left">
+          <div class="space-y-0.5">
+            <p class="text-lg text-black font-semibold">Erin Landform</p>
+            <p class="text-slate-500 font-medium">Product Engineer</p>
+          </div>
+          <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+            Message
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
