5ea4e8080956254a7a6064d5530c6cc442e7832d
Heat Koemnak <heatkimnak@gmail.com>
2023-07-15 22:34:02 +0700

    add ed carousel

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,9 +1,49 @@
 import React from "react";
-
+import { Carousel, initTE } from "tw-elements";
+initTE({ Carousel });
 function Home() {
   return (
     <>
-      <div className="home">Homepage</div>
+      <div
+        id="carouselExampleSlidesOnly"
+        class="relative"
+        data-te-carousel-init
+        data-te-carousel-slide
+      >
+        <div class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+          <div
+            class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-te-carousel-item
+            data-te-carousel-active
+          >
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
+              class="block w-full"
+              alt="Wild Landscape"
+            />
+          </div>
+          <div
+            class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-te-carousel-item
+          >
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
+              class="block w-full"
+              alt="Camera"
+            />
+          </div>
+          <div
+            class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
+            data-te-carousel-item
+          >
+            <img
+              src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
+              class="block w-full"
+              alt="Exotic Fruits"
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
