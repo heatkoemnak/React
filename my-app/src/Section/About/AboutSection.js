6ec98eabd3e1206e456923143789df2777bdc13d
heatkoemnak <heatkimnak@gmail.com>
2023-08-12 00:20:27 +0700

    About Page section

--- my-app/src/Section/About/AboutSection.js
+++ my-app/src/Section/About/AboutSection.js
@@ -7,18 +7,17 @@ import soumany from "../../asset/soumany.jpg";
 import chhaylin from "../../asset/chhaylin.jpg";
 import koemnak from "../../asset/koemnak.jpg";
 import { RxDoubleArrowDown } from "react-icons/rx";
+import { FaFacebook,FaGithub ,FaTwitterSquare} from "react-icons/fa";
+import {AiFillTwitterCircle} from 'react-icons/ai';
 
 import './about.css';
-
+import { IconContext } from 'react-icons';
 
 const Section = styled.div`
-    height: 100vh;
     display:flex;
     flex-direction:column;
     scroll-snap-type: y mandatory;
-
     scroll-behavior: smooth;
-
     overflow-y: auto;
 
     scrollbar-width: none;
@@ -32,7 +31,6 @@ const Section = styled.div`
     }
 `
 const Teams = styled.div`
-    width:100%;
     border-top:2px solid #dbdee6;
     display: flex;
     flex-wrap: wrap;
@@ -40,19 +38,8 @@ const Teams = styled.div`
     background-color: #f5f9f9;
     color: #fff;
     flex-direction: column;
-    align-items: center;
     justify-content: center;
-    div{
-        align-items: center;
-        justify-content: center;
-        padding: 10px;
-        h2{
-            font-size: 1.2rem;
-        }
-        p{
-            font-size: 1rem;
-        }
-    }
+   
     @media screen and(max-width :768px){
         h1{
             font-size: 2rem;
@@ -92,118 +79,165 @@ const Teams = styled.div`
                 }
             p{
                 font-size: 1rem;
-                padding:3% ;
                 }
           }
 
    
 `
 const Team = styled.div`
-
+    width:100%;
     display:flex;
     flex-wrap:wrap;
     align-items:center;
-    background-color:#574a68;
-   
-`
-const Title = styled.div`
-    height:100px;
-    display:flex;
-    flex-direction: column;
-    align-items:center;
     justify-content:center;
-    color: gray;
-
-
-`
-const H2 = styled.div`
-font-size:1.5rem;
-    font-weight: 500;
-    height:50px;
-    text-align: center;
-    font-family: 'Nunito Sans', sans-serif;
-`
-
-const Profile = styled.div`
-    display:flex;
-    background-color:#f6f5f9;
-    margin:10px;
-    padding:30px ;
-    // box-shadow:-4px -8px #e7dcdc,
-    // 8px  16px#cacece,-4px   16px#b
-    // ,8px     24px#aaddad,-4px      24px#
-    // bbbbbb,-4px       32px#99cccc,-4px        3
-    // cccc,-4px          40px#88bbbb,-4px           40
-    // px#77aaaa,-4px         48px#6699aa,-
-    // 4px            56px#558899,-4px             56
-    // px#447788,-4px        64px#336
-    // 99,#225577,-4px               72px#11
-    // 88;#fff;
-
-
-    h2{
-        text-align:left;
-    
-    }
-    
-    
-
-
-
+    background-color:#f7f5ff;
    
 `
+export const Title = styled.div`
+    width: 100%;
+    height: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    h2{
+        color: rgb(53, 53, 53);
+        font-family: 'Poppins', sans-serif;
+        font-size: 1.5rem;
+        font-weight: 500;
+    }
+
+`
+// const H2 = styled.div`
+//     font-size:1.5rem;
+//     font-weight: 500;
+//     height:50px;
+//     color: rgb(53, 53, 53);
+//     text-align: center;
+//     font-family: 'Nunito Sans', sans-serif;
+// `
+const Button = styled.button`
+display;flex;
+    align-items:center;
+    justify-content:space-between;
+    border: none;
+    outline: none;
+    background-color: #23151d;
+    color: #fff;
+    font-weight: 300;
+    padding: 5px 10px;
+    border-radius: 50px;
+    cursor: pointer;
+    transition: all 0.3s ease-in-out;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    &:hover {
+        background-color: #111;
+        color: green;
+    }
+
+
+
+`
+
 
 export default function AboutSection() {
   return (
     <Section>
       <Teams>
         <Title>
-            <H2>Our Team</H2>
-            <RxDoubleArrowDown size='3rem'color='#7cd553'/>
+            <h2>Our Team</h2>
+            <RxDoubleArrowDown size='2rem'color='#7cd553'/>
         </Title>
         <Team>
-            <Profile>
+        
+            <div className='profile'>
                 <div className='image'>
-                    <img src={sopheak} alt=''width={189}/>
+                    <img src={sopheak} alt=''/>
                 </div>
-                <div>
+                <div className='TextInfo' >
                     <h2>Saing Sopheak</h2>
-                    <p>Role</p>
-                    <p>Team Leader, Backend & Server Side</p>
-                    <a href="https://github.com/saingsopheak" target="blank">Github Profile</a>
-                    <button>To Know More</button>
-                </div>
-                
-              
-            </Profile>
-         
-            <Profile>
-              <img src={koemnak} alt=''width={200}/>
-              <h2>HeatKoemnak</h2>
-              <p>Team 2</p>
-            </Profile>
-          
-         
-            <Profile>
-              <img src={soumany} alt=''width={200}/>
-              <h2>Sou Many</h2>
-              <p>Team 3</p>
-            </Profile>
-          
-         
-          <Profile>
-              <img src={sodana} alt=''width={200}/>
-              <h2>Som Sodana</h2>
-              <p>Team 4</p>
-          </Profile>
+                    <span>Role</span>
+                    <p>Backend & Server Side</p>
+                    <div className='icons'>
+                        
+                            <FaFacebook className='icon'/>
+                            <FaGithub className='icon'/>
+                            <FaTwitterSquare className='icon'/>
+                        
 
-          
+                    </div>
+                    <Button>Read More..</Button>
+                </div>
+            </div>
+            <div className='profile'>
+                <div className='image'>
+                    <img src={koemnak} alt=''/>
+                </div>
+                <div className='TextInfo' >
+                    <h2>Heat Koemnak</h2>
+                    <span>Role</span>
+                    <p>Backend and Server</p>
+                    <div className='icons'>
+                            <FaFacebook className='icon'/>
+                            <FaGithub className='icon'/>
+                            <FaTwitterSquare className='icon'/>
+
+                    </div>
+                    <Button>Read More..</Button>
+                </div>
+            </div>
+            <div className='profile'>
+                <div className='image'>
+                    <img src={soumany} alt=''/>
+                </div>
+                <div className='TextInfo' >
+                    <h2>Sou Many</h2>
+                    <span>Role</span>
+                    <p>Frontend and Responsive</p>
          
-            <Profile>
-              <img src={chhaylin} alt=''width={200}/>
-              <h2>Srun ChhayLin</h2>
-              <p>Team 5</p>
-            </Profile>
+                    <div className='icons'>
+                            <FaFacebook className='icon' />
+                            <FaGithub className='icon'/>
+                            <FaTwitterSquare className='icon'/>
+
+                    </div>
+                    <Button>Read More..</Button>
+                </div>
+            </div>
+            <div className='profile'>
+                <div className='image'>
+                    <img src={sodana} alt=''/>
+                </div>
+                <div className='TextInfo' >
+                    <h2>Som Sodana</h2>
+                    <span>Role</span>
+                    <p>Frontend</p>
+                    <div className='icons'>
+                            <FaFacebook className='icon'/>
+                            <FaGithub className='icon'/>
+                            <FaTwitterSquare className='icon'/>
+
+                    </div>
+                    <Button>Read More..</Button>
+                </div>
+            </div>
+            <div className='profile'>
+                <div className='image'>
+                    <img src={chhaylin} alt=''/>
+                </div>
+                <div className='TextInfo' >
+                    <h2>Srun chhaylin</h2>
+                    <span>Role</span>
+                    <p>Check and Hosting</p>
+                    <div className='icons'>
+                            <FaFacebook className='icon'/>
+                            <FaGithub className='icon'/>
+                            <FaTwitterSquare className='icon'/>
+
+                    </div>
+                    <Button>Read More..</Button>
+                </div>
+            </div>
           
         </Team>
        
