5b51ee812d812fe9323580140028c3a183109216
Heat Koemnak <heatkimnak@gmail.com>
2023-08-22 15:14:50 +0700

    cs

--- server/index.js
+++ server/index.js
@@ -1,47 +1,36 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
+const express = require('express');
+const data = require('./data');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-let userList=[
-    {
-        id:1,
-        name:"John",
-        email:"john@gmail.com"
-    },
-   
-]
-
-app.get("/users", (req, res) => {
-    res.json(userList);
-    // res.send("Hello World")
-    
+app.get('users', (req, res) => {
+  res.json(data.team);
 });
-app.post("/users",(req,res)=>{
-    const newUser = req.body;
-    userList.push(newUser);
-    res.json(userList);
-})
-app.put("/users",(req,res)=>{
-    const {id} = req.body;
-    const newUser = req.body;
-    userList.forEach((user,index)=>{
-        if(user.id === id){
-            userList[index]=newUser;
-        }
-    })
-    res.json(userList);
-})
-app.delete("/users",(req,res)=>{
-    const {id} = req.body;
-    userList.forEach((user,index)=>{
-        if(user.id === id){
-            userList.splice(index,1);
-        }
-    })
-    res.json(userList);
-})
-app.listen('3001',(req,res)=>{
-    console.log("Server is running on port 3001")
-
+app.listen('3001', (req, res) => {
+  console.log('Server is running on port 3001');
 });
+// app.post('/users', (req, res) => {
+//   const newUser = req.body;
+//   userList.push(newUser);
+//   res.json(userList);
+// });
+// app.put('/users', (req, res) => {
+//   const { id } = req.body;
+//   const newUser = req.body;
+//   userList.forEach((user, index) => {
+//     if (user.id === id) {
+//       userList[index] = newUser;
+//     }
+//   });
+//   res.json(userList);
+// });
+// app.delete('/users', (req, res) => {
+//   const { id } = req.body;
+//   userList.forEach((user, index) => {
+//     if (user.id === id) {
+//       userList.splice(index, 1);
+//     }
+//   });
+//   res.json(userList);
+// });
