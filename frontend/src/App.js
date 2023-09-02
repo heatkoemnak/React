22ecb6ab141ffbb3dcb31b01bba2a6a370815fae
Heat Koemnak <heatkimnak@gmail.com>
2023-09-02 18:46:46 +0700

    second commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,3 @@
-
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 // import { CartProvider } from './CartContext';
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import './Navbar.css';
+import './navbar.css';
 import logo from '../../assets/new_logo_anti.png';
 
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
-      <nav>
-        <div className="wrap-container">
-          <div className="logo">
-            <img src={logo} alt="logo" />
-          </div>
-          <ul>
-            <li>Home</li>
-            <li>Products</li>
-            <li>Cart</li>
-          </ul>
+    <nav>
+      <div className="wrap-container">
+        <div className="logo">
+          <img src={logo} alt="logo" width={120} />
         </div>
-
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>About</li>
+          <li>FQA</li>
+        </ul>
         <ul>
           <li>Sign In</li>
           <li>Sign Up</li>
         </ul>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 }
 
